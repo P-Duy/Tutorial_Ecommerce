b48805ad126aaaa6ab1783f3e8561e6c0f95a017
PhamVanDuy <vanduy342001@gmail.com>
2023-05-07 14:07:20 +0700

    complete returning to layout products after adding product

--- ecommerce-admin/pages/api/products.js
+++ ecommerce-admin/pages/api/products.js
@@ -1,7 +1,6 @@
-
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
-import mongoose from "mongoose";
+
 
 export default async function handle(req, res) {
     const { method}= req;
