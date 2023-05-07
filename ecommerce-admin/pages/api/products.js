408625ed33496589e218c8fba0a10645bf3029a5
PhamVanDuy <vanduy342001@gmail.com>
2023-05-07 15:18:41 +0700

    complete get id product for edit

--- ecommerce-admin/pages/api/products.js
+++ ecommerce-admin/pages/api/products.js
@@ -5,6 +5,14 @@ import { Product } from "@/models/Product";
 export default async function handle(req, res) {
     const { method}= req;
     await mongooseConnect();
+
+    if (method === "GET"){
+        if(req.query?.id){
+            res.json(await Product.findOne({_id:req.query?.id}))
+        }
+        res.json(await Product.find());
+    }
+
     if (method==='POST') {
         const {title,description,price} = req.body;
         const productDoc= await Product.create({
